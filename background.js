f6ce150ddb2cb331e502f262672278577309bc87
RachitSharma2001 <rachitsharma613@gmail.com>
2021-09-09 16:45:51 -0700

    Finally able to call function from the js of the site!

---
+++ background.js
@@ -0,0 +1,11 @@
+// background.js
+/*
+let color = '#3aa757';
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color });
+  console.log('Default background color set to %cgreen', `color: ${color}`);
+});
+
+<td onclick="javascript:createModalReviewWin('detailWin','Course Summary','course.cfm?crn=23009&amp;termCode=202110&amp;winName=detialWin', 550, 450);" 
+title="View Course Detail" nowrap="nowrap"></td>*/
