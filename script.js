b7b6b8db829f71c8dcf7e752fa8eae7526c2532f
RachitSharma2001 <rachitsharma613@gmail.com>
2021-09-10 11:02:50 -0700

    Code for getting Professor Names

--- script.js
+++ script.js
@@ -1,8 +1,53 @@
-myc("Rachit");
+console.log("We work!");
+
+var target = document.getElementById("inlineCourseResultsDiv");
+
+// create an observer instance
+var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+        professor_name = target.getElementsByClassName("data-item")[0].getElementsByClassName("data-item-long active")[0].getElementsByClassName("float-left")[0].getElementsByClassName("clearfix")[0].getElementsByClassName("data-column")[4];
+        console.log("Inner html: " + professor_name.innerHTML)
+        /*parent_divs = target.getElementsByClassName("data-item");
+        console.log("target class name [0]: " + target.getElementsByClassName("data-item")[0]);
+        console.log(target);
+        for (const child_div in parent_divs){
+            // The first "getbyclassname" returns div with open seats, units, professor
+            // Get the third item from this, the professor
+            console.log("Child div: " + child_div);
+            professors_div = child_div.getElementsByClassName("clearfix")[0].getElementsByClassName("data-column")[2];
+            console.log("Professors div: " + professors_div);
+            console.log("clearfix div: " + child_div.getElementsByClassName("clearfix")[0]);
+        }*/
+        //float_left = target.getElementsByClassName("float-left");
+        //console.log("Float left: " + float_left);
+        /*var arr = [], l = document.links;
+        for(var i=0; i<l.length; i++) {
+            arr.push(l[i].href);
+            console.log("Href: " + l[i].href)
+        }*/
+    });
+});
+
+// configuration of the observer:
+var config = { attributes: true, childList: true, characterData: true }
+
+// pass in the target node, as well as the observer options
+observer.observe(target, config);
+
+/*
+var button = document.getElementsByClassName("btn btn-mini btn-primary uppercase")[0];
+
+button.addEventListener("click", function() {
+    console.log("Clicked!");
+    element = document.getElementById("inlineCourseResultsDiv");
+    console.log(element);
+  });*/
+
+//myc("Rachit");
 //createModalReviewWin('detailWin','Course Summary','course.cfm?crn=23009&amp;termCode=202110&amp;winName=detialWin&amp;ws=n', 550, 450);
-document.search_form = null;
+//document.search_form = null;
 //textSearch();
-ColdFusion.Ajax.submitForm('search_form', 'course_search_results.cfm?crn=23009&amp;termCode=202110&amp;', callback, errorHandler); 
+//ColdFusion.Ajax.submitForm('search_form', 'course_search_results.cfm?crn=23009&amp;termCode=202110&amp;', callback, errorHandler); 
 /*
     New Idea: have an RMP link to the professor show up on all courses. that smallest executable step 
     Then: Maybe try to also link bio, as well as syllabus or helpful information about class. 
@@ -10,10 +55,12 @@ ColdFusion.Ajax.submitForm('search_form', 'course_search_results.cfm?crn=23009&a
     or RMP Easiness ratings. Maybe do this for anything they select -> after Coldfusion returns the results, parse and edit the page to show the classes that are easiest
     from that selection first (or have highest professor ratings). Like if I type ECS 50, and enter search, then Coldfusion loads form, so we add a click listener to 
     search button (or put a button "Sort by easiest professor" or "Sort by highest rated professor") and it edits the list to show classes in taht order
-        So what it does: HAve a button "Sort by easiest professor" and add it to form. Then when user clicks search, and then "Sort by easiest professor", then the 
+        So what it does: HAve a button "Sort by easiest professor" and add it to form. Then when user clicks search, and then the button "Sort by easiest professor", then the 
         code looks at the html and parses professor names, and finds their ratings on rmp, sorts the html div objects, and edit the html to of the search results to show
         in sorted order!
         People would love this.
         Look at the Miguel guys code, how he got RMP ratings, and then see how he searched RMP and do same thing and parse the important details. Hardest part is editing the 
         search results boxes to be in sorted order. Test this first!
+        In end: add button with click listener, which when clicked goes through the html of the search results (look at page source to see what the "boxes" in the search results 
+        when I search are. like what is the id, how can I get professor name, how can change it/reorder it)
 */
