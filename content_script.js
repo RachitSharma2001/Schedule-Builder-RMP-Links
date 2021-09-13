12815f1f2e5c0d45c413d48d78e6fc83234db900
RachitSharma2001 <rachitsharma613@gmail.com>
2021-09-13 10:50:15 -0700

    Adds rmp links when user does add/search courses

--- content_script.js
+++ content_script.js
@@ -8,7 +8,7 @@ function linkRMP(profJson){
     var target = document.getElementById("inlineCourseResultsDiv");
     var genericRmpURL = "https://www.ratemyprofessors.com/ShowRatings.jsp?tid="
     
-    var observer = new MutationObserver(function(mutations) {
+    var inlineObserver = new MutationObserver(function(mutations) {
         mutations.forEach(function(mutation) {
             var genericHrefs = [' (<a href = "', '">RMP</a>)'];
             
@@ -34,7 +34,37 @@ function linkRMP(profJson){
     var config = { attributes: true, childList: true, characterData: true };
 
     // pass in the target node, as well as the observer options
-    observer.observe(target, config);
+    inlineObserver.observe(target, config);
+
+    // Just testing!
+    var outlineTarget = document.getElementById("courseResultsDiv");
+    var outlineObserver = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            var genericHrefs = [' (<a href = "', '">RMP</a>)'];
+            
+            var allClasses = outlineTarget.getElementsByClassName("data-item");
+
+            for(classIndex = 0; classIndex < allClasses.length; classIndex++){
+                var profHrefs = allClasses[classIndex].getElementsByClassName("data-item-long active")[0].getElementsByClassName("float-left")[0].getElementsByClassName("clearfix")[0].getElementsByClassName("data-column")[4];
+                var profTags = profHrefs.getElementsByTagName("a");
+                for(i = 0; i < profTags.length; i++){
+                    var profName = profHrefs.getElementsByTagName("a")[i].innerHTML;
+                    var profNameInFormat = getInFormat(profName);
+                    var profTid = profJson[profNameInFormat];
+                    
+                    var specificRmpUrl = genericRmpURL + profJson[profNameInFormat];
+                    var specificHref = genericHrefs[0] + specificRmpUrl + genericHrefs[1];
+                    profHrefs.getElementsByTagName("a")[i++].innerHTML += specificHref;
+                }
+            }
+        });
+    });
+
+    // configuration of the observer:
+    var outlineConfig = { attributes: true, childList: true, characterData: true };
+
+    // pass in the target node, as well as the observer options
+    outlineObserver.observe(outlineTarget, outlineConfig);
 }
 
 const url = chrome.runtime.getURL('test_file.txt');
