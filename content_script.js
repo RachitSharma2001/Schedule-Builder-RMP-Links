17619a71b34dec28829a665d4633d56d6e9d43b6
RachitSharma2001 <rachitsharma613@gmail.com>
2021-09-12 21:24:45 -0700

    Have unfinished construction of code to combine getting the tids with inserting them into the schedule builder code

--- content_script.js
+++ content_script.js
@@ -14,36 +14,74 @@
 */
 
 /*
-const url = chrome.runtime.getURL('test_file.txt');
+var prof_tid;
+
+function getProfessorTID(url, name){
+    fetch(url)
+    .then((response) => response.json()) //assuming file contains json
+    .then((json) => prof_tid = json[name]);
+}*/
+
 
 // url to link to: https://www.ratemyprofessors.com/ShowRatings.jsp?tid=2503455
 
-
+/*
 fetch(url)
     .then((response) => response.json()) //assuming file contains json
-    .then((json) => console.log("Here is json: " + json["G_Quon"]));
+    .then((json) => function f(){ 
+                
+        var target = document.getElementById("inlineCourseResultsDiv");
+
+        // create an observer instance
+        var observer = new MutationObserver(function(mutations) {
+            mutations.forEach(function(mutation) {
+                professor_name = target.getElementsByClassName("data-item")[0].getElementsByClassName("data-item-long active")[0].getElementsByClassName("float-left")[0].getElementsByClassName("clearfix")[0].getElementsByClassName("data-column")[4];
+                console.log("Inner html: " + professor_name.innerHTML);
+                var name = "R_Furrow";
+                //professor_name.innerHTML = '<a href="mailto:refurrow@ucdavis.edu">Inserted!</a>, <a href="mailto:slkeen@ucdavis.edu">Another insert!</a>';
+                console.log(json[name]);
+        });
+
+        // configuration of the observer:
+        var config = { attributes: true, childList: true, characterData: true }
+
+        // pass in the target node, as well as the observer options
+        observer.observe(target, config);
+    });
+});*/
 
 
+/* 
+    TODO: 
+    1. See how to use wait() function
+    2. wait until tids is not undefined 
+    3. Then go about inserting all the urls -> https://appdividend.com/2020/07/31/javascript-wait-how-to-make-function-wait-in-javascript/
+*/
 
+tids = undefined;
 
-var script = document.createElement("script");
-script.src = chrome.runtime.getURL("edit_html.js");
-(document.head || document.documentElement).appendChild(script);*/
+const url = chrome.runtime.getURL('test_file.txt');
+fetch(url)
+    .then((response) => response.json()) //assuming file contains json
+    .then((json) => tid = json);
 
+// replace with a wait function that waits till tids is not undefined
+while(tids == undefined){
+}
 var target = document.getElementById("inlineCourseResultsDiv");
 
 // create an observer instance
 var observer = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
         professor_name = target.getElementsByClassName("data-item")[0].getElementsByClassName("data-item-long active")[0].getElementsByClassName("float-left")[0].getElementsByClassName("clearfix")[0].getElementsByClassName("data-column")[4];
-        console.log("Inner html: " + professor_name.innerHTML)
-        professor_name.innerHTML = '<a href="mailto:refurrow@ucdavis.edu">Inserted!</a>, <a href="mailto:slkeen@ucdavis.edu">Another insert!</a>';
+        console.log("Inner html: " + professor_name.innerHTML);
+        var name = "R_Furrow";
+        //professor_name.innerHTML = '<a href="mailto:refurrow@ucdavis.edu">Inserted!</a>, <a href="mailto:slkeen@ucdavis.edu">Another insert!</a>';
+        console.log(json[name]);
     });
 });
-
 // configuration of the observer:
-var config = { attributes: true, childList: true, characterData: true }
+var config = { attributes: true, childList: true, characterData: true };
 
 // pass in the target node, as well as the observer options
 observer.observe(target, config);
-
