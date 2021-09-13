357b809a8e48e1754279f5c42079b7daaaa5db9a
RachitSharma2001 <rachitsharma613@gmail.com>
2021-09-13 08:34:43 -0700

    Am able to get the professor tid json first and then change elements

--- content_script.js
+++ content_script.js
@@ -58,6 +58,61 @@ fetch(url)
     3. Then go about inserting all the urls -> https://appdividend.com/2020/07/31/javascript-wait-how-to-make-function-wait-in-javascript/
 */
 
+/*
+function getProfessorJson(url){
+    prof_json = undefined;
+    fetch(url)
+    .then((response) => response.json()) //assuming file contains json
+    .then((json) => prof_json = json);
+    if(prof_json == undefined){
+        setTimeout(getProfessorJson, 300, url);
+    }else{
+        return prof_json;
+    }
+}
+ 
+
+prof_json = getProfessorJson(chrome.runtime.getURL('test_file.txt'));
+if(prof_json == undefined){
+    setTimeout(() => {
+        console.log("Professor json for A_Kaloti: ", prof_json["A_Kaloti"]);
+    }, 300);
+}*/
+
+function linkRMP(profJson){
+    //console.log("Professor json for A_Kaloti: ", profJson["A_Kaloti"]);
+    var target = document.getElementById("inlineCourseResultsDiv");
+
+    // create an observer instance
+    var observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            professor_name = target.getElementsByClassName("data-item")[0].getElementsByClassName("data-item-long active")[0].getElementsByClassName("float-left")[0].getElementsByClassName("clearfix")[0].getElementsByClassName("data-column")[4];
+            console.log("Inner html: " + professor_name.innerHTML);
+            var name = "R_Furrow";
+            professor_name.innerHTML = '<a href="mailto:refurrow@ucdavis.edu">Inserted!</a>, <a href="mailto:slkeen@ucdavis.edu">Prof json of Kaloti: ' + profJson["A_Kaloti"] + '</a>';
+            console.log(json[name]);
+        });
+    });
+    // configuration of the observer:
+    var config = { attributes: true, childList: true, characterData: true };
+
+    // pass in the target node, as well as the observer options
+    observer.observe(target, config);
+}
+
+const url = chrome.runtime.getURL('test_file.txt');
+    fetch(url)
+    .then((response) => response.json()) //assuming file contains json
+    .then((json) => linkRMP(json));
+
+/*
+(async () => {
+    const url = chrome.runtime.getURL('test_file.txt');
+    var profTids = await fetch(url).then((response) => response.json());
+    console.log("Professor json for A_Kaloti: ", prof_json["A_Kaloti"]);
+});*/
+//console.log("Professor json for A_Kaloti: ", prof_json["A_Kaloti"]);
+/*
 tids = undefined;
 
 const url = chrome.runtime.getURL('test_file.txt');
@@ -66,8 +121,7 @@ fetch(url)
     .then((json) => tid = json);
 
 // replace with a wait function that waits till tids is not undefined
-while(tids == undefined){
-}
+
 var target = document.getElementById("inlineCourseResultsDiv");
 
 // create an observer instance
@@ -76,7 +130,7 @@ var observer = new MutationObserver(function(mutations) {
         professor_name = target.getElementsByClassName("data-item")[0].getElementsByClassName("data-item-long active")[0].getElementsByClassName("float-left")[0].getElementsByClassName("clearfix")[0].getElementsByClassName("data-column")[4];
         console.log("Inner html: " + professor_name.innerHTML);
         var name = "R_Furrow";
-        //professor_name.innerHTML = '<a href="mailto:refurrow@ucdavis.edu">Inserted!</a>, <a href="mailto:slkeen@ucdavis.edu">Another insert!</a>';
+        professor_name.innerHTML = '<a href="mailto:refurrow@ucdavis.edu">Inserted!</a>, <a href="mailto:slkeen@ucdavis.edu">Another insert!</a>';
         console.log(json[name]);
     });
 });
@@ -85,3 +139,4 @@ var config = { attributes: true, childList: true, characterData: true };
 
 // pass in the target node, as well as the observer options
 observer.observe(target, config);
+*/
